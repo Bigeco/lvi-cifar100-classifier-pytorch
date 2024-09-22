a9a66fd50d9cc9dbdd0a5a4138c5b796984a094a
Bigeco <bigeco173@gmail.com>
2024-09-22 15:40:23 +0900

    Feat: add load_data function

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,7 +1,7 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -27,13 +27,46 @@ class CifarDataset(Dataset):
         plt.imshow(npimg)
         plt.show() 
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
 
-trainset = CifarDataset(root='./data', transform=transform, train=True)
-testset = CifarDataset(root='./data', transform=transform, train=False)
+def load_data(batch_size, num_workers=2, train_ratio=0.8, root='./data'):
+    """
+    Load and prepare CIFAR100 dataset.
+    
+    Args:
+        batch_size (int): Size of each batch.
+        num_workers (int): Number of subprocesses to use for data loading.
+        train_ratio (float): Ratio of training set to full dataset.
+        root (str): Root directory of dataset.
+    
+    Returns:
+        tuple: (train_loader, valid_loader)
+    """
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    
+    full_dataset = CifarDataset(root='./data', transform=transform, train=True)
+    
+    dataset_size = len(full_dataset)
+    train_size = int(train_ratio * dataset_size)
+    valid_size = dataset_size - train_size
+    
+    trainset, validset = random_split(full_dataset, [train_size, valid_size])
 
-trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
-testloader = DataLoader(testset, batch_size=4, shuffle=True, num_workers=2)
+    train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    valid_loader = DataLoader(validset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_loader, valid_loader
+
+if __name__ == "__main__":
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    trainset = CifarDataset(root='./data', transform=transform, train=True)
+    testset = CifarDataset(root='./data', transform=transform, train=False)
+
+    trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+    testloader = DataLoader(testset, batch_size=4, shuffle=True, num_workers=2)
