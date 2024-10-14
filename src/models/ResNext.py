0f934b2beac2dca9a7c49a4249d5ec49cf464f31
Song-Daeun <115616989+Song-Daeun@users.noreply.github.com>
2024-10-14 22:58:59 +0900

    feat: add ResNext.py

--- src/models/ResNext.py
+++ src/models/ResNext.py
@@ -1,3 +1,80 @@
-# TODO 1: 모델 클래스 코드 추가
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-# TODO 2: 모델 클래스 객체를 선언하는 함수 추가
+class ResNextBottleNeckC(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
+        super().__init__()
+
+        C = CARDINALITY 
+
+        D = int(DEPTH * out_channels / BASEWIDTH)
+        self.split_transforms = nn.Sequential(
+            nn.Conv2d(in_channels, C * D, kernel_size=1, groups=C, bias=False),
+            nn.BatchNorm2d(C * D),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(C * D, C * D, kernel_size=3, stride=stride, groups=C, padding=1, bias=False),
+            nn.BatchNorm2d(C * D),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(C * D, out_channels * 4, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels * 4),
+        )
+
+        self.shortcut = nn.Sequential()
+
+        if stride != 1 or in_channels != out_channels * 4:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * 4, stride=stride, kernel_size=1, bias=False),
+                nn.BatchNorm2d(out_channels * 4)
+            )
+
+    def forward(self, x):
+        return F.relu(self.split_transforms(x) + self.shortcut(x))
+
+class ResNext(nn.Module):
+    def __init__(self, block, num_blocks, class_names=100):
+        super().__init__()
+        self.in_channels = 64
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+
+        self.conv2 = self._make_layer(block, num_blocks[0], 64, 1)
+        self.conv3 = self._make_layer(block, num_blocks[1], 128, 2)
+        self.conv4 = self._make_layer(block, num_blocks[2], 256, 2)
+        self.conv5 = self._make_layer(block, num_blocks[3], 512, 2)
+        self.avg = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * 4, 100)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.avg(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+    def _make_layer(self, block, num_block, out_channels, stride):
+        strides = [stride] + [1] * (num_block - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels * 4
+
+        return nn.Sequential(*layers)
+
+def resnext50():
+    return ResNext(ResNextBottleNeckC, [3, 4, 6, 3])
+
+def resnext101():
+    return ResNext(ResNextBottleNeckC, [3, 4, 23, 3])
+
+def resnext152():
+    return ResNext(ResNextBottleNeckC, [3, 4, 36, 3])
